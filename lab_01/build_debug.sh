0398ab9d69ae33e08f841fcff38b354bb6dbb6df
dlankinl <dmit.lankin@mail.ru>
2022-10-08 13:10:29 +0300

    Final version.

--- lab_01/build_debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-gcc -c arr_file.c -std=c99 -Wall -Werror -Wpedantic -Wextra -Wfloat-equal -Wfloat-conversion -ggdb
-
-gcc -c items.c -std=c99 -Wall -Werror -Wpedantic -Wextra -Wfloat-equal -Wfloat-conversion -ggdb
-
-gcc -c main.c -std=c99 -Wall -Werror -Wpedantic -Wextra -Wfloat-equal -Wfloat-conversion -ggdb
-gcc -o app.exe main.o items.o arr_file.o -lm
--- lab_01/build_release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gcc -c main.c -Wall -Werror -Wpedantic -Wextra -Wfloat-equal -Wfloat-conversion
-g++ -o app.exe main.o -lm
