15d3ca994390e6ac4bb2246ec0477481264c9022
dlankinl <dmit.lankin@mail.ru>
2022-10-15 14:45:34 +0300

    Random filling of sparse matrix was added.

---
+++ lab_03/rndm_sparse_matr.c
@@ -0,0 +1,6 @@
+#include "../inc/rndm_sparse_matr.h"
+
+// size_t rndm_sprs_matr_fill(sparse_matrix *matr)
+// {
+//     printf("")
+// }
---
+++ lab_03/rndm_sparse_matr.h
@@ -0,0 +1,10 @@
+#ifndef __RNDM_SPARSE_MATR_H__
+#define __RNDM_SPARSE_MATR_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "structs.h"
+#include "errors.h"
+
+
+#endif // __RNDM_SPARSE_MATR_H__
